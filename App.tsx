c6090c05981bc8e80870a6184e2e1299ab7df46f
abdulrahmanbashir7890-hue <abdulrahmanbashir7890@gmail.com>
2026-08-01 21:42:57 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router-dom";
 
 import SplashPage from "./pages/SplashPage";
 import WelcomePage from "./pages/WelcomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SplashPage />} />
       <Route path="/welcome" element={<WelcomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
     </Routes>
   );
 }
