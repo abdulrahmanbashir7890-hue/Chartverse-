779b08cf1199be346599c9abe55cda34aa1fc3b0
abdulrahmanbashir7890-hue <abdulrahmanbashir7890@gmail.com>
2026-08-01 22:09:28 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,18 +1,38 @@
+import { Routes, Route } from "react-router-dom";
 
 import SplashPage from "./pages/SplashPage";
 import WelcomePage from "./pages/WelcomePage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import OTPVerificationPage from "./pages/OTPVerificationPage";
+import PhoneLoginPage from "./pages/PhoneLoginPage";
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<SplashPage />} />
-      <Route path="/welcome" element={<WelcomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-    </Routes>
-  );
+export default function App(){
+
+return(
+
+<Routes>
+
+<Route path="/" element={<SplashPage/>}/>
+
+<Route path="/welcome" element={<WelcomePage/>}/>
+
+<Route path="/login" element={<LoginPage/>}/>
+
+<Route path="/signup" element={<SignupPage/>}/>
+
+<Route path="/forgot-password" element={<ForgotPasswordPage/>}/>
+
+<Route path="/reset-password" element={<ResetPasswordPage/>}/>
+
+<Route path="/otp" element={<OTPVerificationPage/>}/>
+
+<Route path="/phone-login" element={<PhoneLoginPage/>}/>
+
+</Routes>
+
+);
+
 }
-
-export default App;
